5303bb9ea9b8dae729d7942474062071e75a1b24
miroshnikoff84 <miroshnikoff84@mail.ru>
2020-10-02 02:02:18 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let arr = str.split('');
+    let reversArray = [];
+
+    for(let i = str.length-1; i >= 0; i--) {
+        let aaa = arr[i];
+        reversArray.push(arr[i]);
+    }
+
+    return parseInt(reversArray.join(''));
 }
